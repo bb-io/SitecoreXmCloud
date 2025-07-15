0c8726241104c80e954c8b03aabde6e43e8979eb
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-07-15 11:12:05 +0300

    feat(workflow): ✨ enhance `UpdateWorkflowState` method; add locale and version parameters; implement validation for workflow commands; add unit tests for workflow actions

---
+++ Tests.SitecoreXmCloud/WorkflowActionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Apps.Sitecore.Actions;
+using Apps.SitecoreXmCloud.Models.Requests.Workflows;
+using Tests.Sitecore.Base;
+
+namespace Tests.Sitecore;
+
+[TestClass]
+public class WorkflowActionsTests : TestBase
+{
+    [TestMethod]
+    public async Task UpdateWorkflowState_ValidData_ShouldUpdateWorkflowState()
+    {
+        // Arrange
+        var request = new UpdateWorkflowStateRequest
+        {
+            ItemId = "481b20da-bcbf-4676-aaa7-3844824521f7",
+            WorkflowCommandId = "65EF04A8-E6E0-45D3-B28B-69793F264193",
+            Version = "2",
+            Locale = "de"
+        };
+
+        var action = new WorkflowActions(InvocationContext);
+
+        // Act
+        var response = await action.UpdateWorkflowState(request);
+
+        // Assert
+        Assert.IsNotNull(response);
+    }
+}
