b7c6abbfd9630e3d24a67e956e8c3c711f7772fd
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-07-15 16:09:07 +0300

    feat(connections): ✨ enhance `ConnectionValidator` for improved error handling; refactor validation logic to streamline response processing; add checks for empty responses and provide clearer error messages

--- Apps.SitecoreXmCloud/Connections/ConnectionValidator.cs
+++ Apps.SitecoreXmCloud/Connections/ConnectionValidator.cs
@@ -15,31 +15,11 @@ public class ConnectionValidator : IConnectionValidator
         try
         {
             var creds = authProviders.ToArray();
-            
-            var client = new SitecoreClient(creds);
-            var request = new SitecoreRequest("/Locales", Method.Get, creds);
+            var response = await ExecuteValidationRequest(creds);
 
-            var response = await client.ExecuteAsync(request);
-
-            if (!response.IsSuccessStatusCode) 
+            if (!response.IsSuccessStatusCode)
             {
-                try
-                {
-                    var error = JsonConvert.DeserializeObject<MessageResponse>(response.Content!)!;
-                    return new()
-                    {
-                        IsValid = false,
-                        Message = $"The Sitecore server returned the following message: {error.Message}. Please verify if the plugin was installed correctly."
-                    };
-                } catch (Exception ex)
-                {
-                    return new()
-                    {
-                        IsValid = false,
-                        Message = response.StatusDescription
-                    };
-                }
-
+                return HandleErrorResponse(response);
             }
 
             return new()
@@ -53,6 +33,52 @@ public class ConnectionValidator : IConnectionValidator
             {
                 IsValid = false,
                 Message = ex.Message
+            };
+        }
+    }
+    
+    private async Task<RestResponse> ExecuteValidationRequest(AuthenticationCredentialsProvider[] creds)
+    {
+        var client = new SitecoreClient(creds);
+        var request = new SitecoreRequest("/Locales", Method.Get, creds);
+        return await client.ExecuteAsync(request);
+    }
+    
+    private ConnectionValidationResponse HandleErrorResponse(RestResponse response)
+    {
+        try
+        {
+            if(string.IsNullOrEmpty(response.Content))
+            {
+                if (string.IsNullOrEmpty(response.ErrorMessage))
+                {
+                    return new()
+                    {
+                        IsValid = false,
+                        Message = "The Sitecore server returned an empty response. Please verify the URL you provided, and ensure it accessible and doesn't have firewall restrictions."
+                    };
+                }
+                
+                return new()
+                {
+                    IsValid = false,
+                    Message = response.ErrorMessage
+                };
+            }
+            
+            var error = JsonConvert.DeserializeObject<MessageResponse>(response.Content!)!;
+            return new()
+            {
+                IsValid = false,
+                Message = $"The Sitecore server returned the following message: {error.Message}. Please verify if the plugin was installed correctly."
+            };
+        } 
+        catch (Exception)
+        {
+            return new()
+            {
+                IsValid = false,
+                Message = response.ErrorMessage
             };
         }
     }
